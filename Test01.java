c388d0c1b0ea7890b0fca64f268bcabf810fb59a
gLuis462 <Gonza462@uwm.edu>
2016-12-26 17:56:09 -0600

    Initial commit

---
+++ Test01.java
@@ -0,0 +1,10 @@
+package Test02;
+
+/**
+ * Created by LGonzalez on 12/26/16.
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("yes it worked");
+    }
+}
